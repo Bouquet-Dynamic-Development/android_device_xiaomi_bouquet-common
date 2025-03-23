12f052b0e724e2b2515764f3247f918f0cfba6d1
NopeNopeGuy <kingdutchisbad+12@proton.me>
2025-03-23 17:47:29 +0530

    fixup! bouquet-common: Move to USB AIDL hal

--- bouquet.mk
+++ bouquet.mk
@@ -301,7 +301,8 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 # USB
-PRODUCT_PACKAGES += usb_compositions.conf \
+PRODUCT_PACKAGES += \
+    usb_compositions.conf \
     android.hardware.usb-service.qti \
     android.hardware.usb.gadget-service.qti
 
