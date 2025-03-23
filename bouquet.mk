77253db49bf1a67289fd97b718b47a3286a11b88
duckyduckg <duckyduckg65@gmail.com>
2025-03-23 12:44:02 +0500

    bouquet-common: commonize usb hal

--- bouquet.mk
+++ bouquet.mk
@@ -455,6 +455,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.basic
+
 # Updater
 AB_OTA_UPDATER := false
 
