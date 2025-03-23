668a9ce2bc38bdaeceafaf15e7d607e08e6d8f47
NopeNopeGuy <kingdutchisbad+12@proton.me>
2025-03-23 23:58:29 +0530

    Revert "bouquet-common: Move to USB AIDL hal"
    This reverts commit 48f4531784ec851986f1497c2fa13d3c1665e3f2.

--- bouquet.mk
+++ bouquet.mk
@@ -301,11 +301,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
-# USB
-PRODUCT_PACKAGES += usb_compositions.conf \
-    android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti
-
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
@@ -463,6 +458,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.basic
 
 # Updater
 AB_OTA_UPDATER := false
